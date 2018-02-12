fe95e469996daab2b14fcfe965c18022d24838f5
Erika Smith <erikaannesmith@gmail.com>
2018-02-12 11:51:11 -0700

    npm run build

--- docs/bundle.js
+++ docs/bundle.js
@@ -73,18 +73,93 @@ module.exports = __webpack_require__(3);
 
 /***/ }),
 /* 1 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports, __webpack_require__) {
 
-"use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_jquery__ = __webpack_require__(2);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_jquery___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_jquery__);
+const APIhost = `https://wordwatch-api.herokuapp.com`
+var $ = __webpack_require__(2);
 
-
-__WEBPACK_IMPORTED_MODULE_0_jquery___default()(document).ready(() => {
-  // have fun!
+$(document).ready(() => {
+  getTopWord()
+  $("textarea")[0]
+  .addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+      $("button").click()
+    }
+  })
+  $('button').on('click', breakDown)
 })
 
+const getTopWord = () => {
+  return fetch(APIhost + `/api/v1/top_word`)
+    .then(response => handleResponse(response))
+    .then(topWord => addTopWord(topWord))
+    .catch(error => console.log( {error} ))
+}
+
+const handleResponse = (response) => {
+  return response.json()
+  .then(json => {
+    if (!response.ok) {
+      const error = {
+        status: response.status,
+        statusText: response.statusText,
+        json,
+      }
+      return Promise.reject(error)
+    }
+    return json
+  })
+}
+
+const addTopWord = (topWord) => {
+  let word = Object.keys(topWord.word)[0]
+  let count = Object.values(topWord.word)[0]
+  $('h3').append(`${word} (${count})`)
+}
+
+const getWordCount = (wordList) => {
+  let wordCount = {}
+  wordList.map(function(word) {
+    if (!wordCount[word]) {
+      wordCount[word] = 1
+    }
+    else {
+      wordCount[word] += 1
+    }
+  })
+  return wordCount
+}
+
+const breakDown = () => {
+  event.preventDefault
+  let wordList = $('textarea')[0].value.replace(/\n/g, "").split(' ')
+  let wordCount = getWordCount(wordList)
+  Object.entries(wordCount).forEach(function(word) {
+    appendToWordCount(word)
+    for (var i = 0; i < word[1]; i++) {
+      addWord(word)
+    }
+  })
+}
+
+const appendToWordCount = (word) => {
+  $('.word-count').append(`<p class=${word[0]}>${word[0]}</p>`)
+  $(`.${word[0]}`).css("fontSize", `${word[1]}em`)
+}
+
+const addWord = (word) => {
+  let wordName = word[0]
+  let data = { word: { value: word[0] } }
+    return fetch(APIhost + `/api/v1/words`, {
+      method: 'post',
+      headers:
+      { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    })
+    .then(response => handleResponse(response))
+    .catch(error => console.log( {error} ))
+}
 
 /***/ }),
 /* 2 */
